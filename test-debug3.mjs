acd8081ba66b526b1532b762bc028a49be022254
00080000 <3388065969@qq.com>
2026-08-30 19:58:44 +0800

    chore: 移除调试文件

--- test-debug3.mjs
+++
@@ -1,6 +0,0 @@
-import { extractTypeSignature } from './src/symbols.js'
-
-// Test the joined text directly
-const joined = `function multiLine(     a: string,     b: number   ): Promise<void> { }`
-console.log("Input:", JSON.stringify(joined))
-console.log("Output:", JSON.stringify(extractTypeSignature(joined)))
